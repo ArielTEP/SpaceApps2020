803bb897fb4268fad4272bccf570747ed9834f16
peguerosdc <peguerosdc@gmail.com>
2020-09-24 21:40:18 -0500

    Drafting UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import Map2D from './components/map'
+import Log from './components/log'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: "flex", flexDirection:"row"}}>
+      <div style={{flex:1}}>
+        <Map2D />
+      </div>
+      <div style={{flex:1}}>
+        <Log />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
