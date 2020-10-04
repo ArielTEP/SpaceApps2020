8ef42ad757e998589311c5478498abe99b5dcefc
peguerosdc <peguerosdc@gmail.com>
2020-10-04 15:56:47 -0500

    Adding systems to splash

---
+++ frontend/src/components/splash/engines.js
@@ -0,0 +1,291 @@
+const warp_dive = {
+  id: 6,
+  name: "WARP DRIVE",
+  description: "Remember Star Trek? Miguel Alcubierre theorised that it is possible to navigate through the universe by folding the space-time right behind you to create thrust. It should be possible to travel faster than the speed of light and, well, according to Einstein's equations... it should work!",
+  image: "/spacecrafts/sc6.png",
+  minigame1 : {
+    parts : [
+      {
+        base : "/minigame1/parts_sc6/sc6p0.png",
+        target : "/minigame1/parts_sc6/sc6p0.target.png",
+        targetWidth: 500,
+        targetHeight: 500
+      },
+      {
+        base : "/minigame1/parts_sc6/sc6p1.png",
+        target : "/minigame1/parts_sc6/sc6p1.target.png",
+        targetWidth: 205,
+        targetHeight: 625
+      },
+      {
+        base : "/minigame1/parts_sc6/sc6p2.png",
+        target : "/minigame1/parts_sc6/sc6p2.target.png",
+        targetWidth: 220,
+        targetHeight: 650
+      },
+      {
+        base : "/minigame1/parts_sc6/sc6p3.png",
+        target : "/minigame1/parts_sc6/sc6p3.target.png",
+        targetWidth: 230,
+        targetHeight: 650
+      },
+      {
+        base : "/minigame1/parts_sc6/sc6p4.png",
+        target : "/minigame1/parts_sc6/sc6p4.target.png",
+        targetWidth: 500,
+        targetHeight: 780
+      },
+    ]
+  }
+}
+
+const rocket_engine = {
+  id: 1,
+  name: "ROCKET ENGINE",
+  description: "these guys have been around since the space-race began. They produce thrust via Newton's third law of motion by ejecting mass in the opposite direction of movement. This requires a lot of energy and is usually produced by combustion.",
+  image: "/spacecrafts/sc1.png",
+  minigame1 : {
+    parts : [
+      {
+        base : "/minigame1/parts_sc1/sc1p0.png",
+        target : "/minigame1/parts_sc1/sc1p0.target.png",
+        targetWidth: 342,
+        targetHeight: 600
+      },
+      {
+        base : "/minigame1/parts_sc1/sc1p1.png",
+        target : "/minigame1/parts_sc1/sc1p1.target.png",
+        targetWidth: 282,
+        targetHeight: 594
+      },
+      {
+        base : "/minigame1/parts_sc1/sc1p2.png",
+        target : "/minigame1/parts_sc1/sc1p2.target.png",
+        targetWidth: 144,
+        targetHeight: 534
+      },
+      {
+        base : "/minigame1/parts_sc1/sc1p3.png",
+        target : "/minigame1/parts_sc1/sc1p3.target.png",
+        targetWidth: 72,
+        targetHeight: 408
+      },
+      {
+        base : "/minigame1/parts_sc1/sc1p4.png",
+        target : "/minigame1/parts_sc1/sc1p4.target.png",
+        targetWidth: 342,
+        targetHeight: 792
+      },
+      {
+        base : "/minigame1/parts_sc1/sc1p5.png",
+        target : "/minigame1/parts_sc1/sc1p5.target.png",
+        targetWidth: 342,
+        targetHeight: 798
+      },
+    ]
+  }
+}
+
+const nuclear_propulsion = {
+  id: 2,
+  name: "NUCLEAR PROPULSION",
+  "description": "Travelling through the universe requires a lot of energy and right now, one of our best source of usable energy is by nuclear reactors. These are particularly useful where the spacecraft is required to be autonomous for a long time without access to other sources of fuel. Energy is produced by the decay of radioactive materials which can be converted to electrical power.",
+  image: "/spacecrafts/sc2.png",
+  minigame1 : {
+    parts : [
+      {
+        base : "/minigame1/parts_sc2/sc2p0.png",
+        target : "/minigame1/parts_sc2/sc2p0.target.png",
+        targetWidth: 116,
+        targetHeight: 561
+      },
+      {
+        base : "/minigame1/parts_sc2/sc2p1.png",
+        target : "/minigame1/parts_sc2/sc2p1.target.png",
+        targetWidth: 72,
+        targetHeight: 281
+      },
+      {
+        base : "/minigame1/parts_sc2/sc2p2.png",
+        target : "/minigame1/parts_sc2/sc2p2.target.png",
+        targetWidth: 116,
+        targetHeight: 308
+      },
+      {
+        base : "/minigame1/parts_sc2/sc2p3.png",
+        target : "/minigame1/parts_sc2/sc2p3.target.png",
+        targetWidth: 72,
+        targetHeight: 281
+      },
+      {
+        base : "/minigame1/parts_sc2/sc2p4.png",
+        target : "/minigame1/parts_sc2/sc2p4.target.png",
+        targetWidth: 176,
+        targetHeight: 396
+      }
+    ]
+  }
+}
+
+const solar_sails = {
+  id: 3,
+  name: "SOLAR SAILS",
+  description: "In the Earth the atmosphere absorbs most of UV radiation from the Sun to protect us, but in the space that's not the case... which is a good thing! Radiation pressure exerted by sunlight on large mirrors can be used to create thrust the same way a boat uses wind and a sail no navigate in the ocean.",
+  image: "/spacecrafts/sc3.png",
+  minigame1 : {
+    parts : [
+      {
+        base : "/minigame1/parts_sc3/sc3p0.png",
+        target : "/minigame1/parts_sc3/sc3p0.target.png",
+        targetWidth: 255,
+        targetHeight: 612
+      },
+      {
+        base : "/minigame1/parts_sc3/sc3p1.png",
+        target : "/minigame1/parts_sc3/sc3p1.target.png",
+        targetWidth: 790,
+        targetHeight: 375
+      },
+      {
+        base : "/minigame1/parts_sc3/sc3p2.png",
+        target : "/minigame1/parts_sc3/sc3p2.target.png",
+        targetWidth: 224,
+        targetHeight: 616
+      },
+      {
+        base : "/minigame1/parts_sc3/sc3p3.png",
+        target : "/minigame1/parts_sc3/sc3p3.target.png",
+        targetWidth: 325,
+        targetHeight: 710
+      },
+      {
+        base : "/minigame1/parts_sc3/sc3p4.png",
+        target : "/minigame1/parts_sc3/sc3p4.target.png",
+        targetWidth: 525,
+        targetHeight: 612
+      },
+      {
+        base : "/minigame1/parts_sc3/sc3p5.png",
+        target : "/minigame1/parts_sc3/sc3p5.target.png",
+        targetWidth: 525,
+        targetHeight: 792
+      },
+      {
+        base : "/minigame1/parts_sc3/sc3p6.png",
+        target : "/minigame1/parts_sc3/sc3p6.target.png",
+        targetWidth: 567,
+        targetHeight: 792
+      }
+    ]
+  }
+}
+
+const ion_engine = {
+  id: 4,
+  name: "ION ENGINE",
+  description: "TODO",
+  image: "/spacecrafts/sc4.png",
+  minigame1 : {
+    parts : [
+      {
+        base : "/minigame1/parts_sc4/sc4p0.png",
+        target : "/minigame1/parts_sc4/sc4p0.target.png",
+        targetWidth: 72,
+        targetHeight: 232
+      },
+      {
+        base : "/minigame1/parts_sc4/sc4p1.png",
+        target : "/minigame1/parts_sc4/sc4p1.target.png",
+        targetWidth: 64,
+        targetHeight: 472
+      },
+      {
+        base : "/minigame1/parts_sc4/sc4p2.png",
+        target : "/minigame1/parts_sc4/sc4p2.target.png",
+        targetWidth: 48,
+        targetHeight: 176
+      },
+      {
+        base : "/minigame1/parts_sc4/sc4p3.png",
+        target : "/minigame1/parts_sc4/sc4p3.target.png",
+        targetWidth: 40,
+        targetHeight: 120
+      },
+      {
+        base : "/minigame1/parts_sc4/sc4p4.png",
+        target : "/minigame1/parts_sc4/sc4p4.target.png",
+        targetWidth: 104,
+        targetHeight: 528
+      },
+      {
+        base : "/minigame1/parts_sc4/sc4p5.png",
+        target : "/minigame1/parts_sc4/sc4p5.target.png",
+        targetWidth: 72,
+        targetHeight: 584
+      },
+      {
+        base : "/minigame1/parts_sc4/sc4p6.png",
+        target : "/minigame1/parts_sc4/sc4p6.target.png",
+        targetWidth: 104,
+        targetHeight: 272
+      },
+      {
+        base : "/minigame1/parts_sc4/sc4p7.png",
+        target : "/minigame1/parts_sc4/sc4p7.target.png",
+        targetWidth: 104,
+        targetHeight: 664
+      }
+    ]
+  }
+}
+
+const antimatter = {
+  id: 5,
+  name: "ANTI-MATTER ENGINE",
+  description: "TODO",
+  image: "/spacecrafts/sc5.png",
+  minigame1 : {
+    parts : [
+      {
+        base : "/minigame1/parts_sc5/sc5p0.png",
+        target : "/minigame1/parts_sc5/sc5p0.target.png",
+        targetWidth: 173,
+        targetHeight: 631
+      },
+      {
+        base : "/minigame1/parts_sc5/sc5p1.png",
+        target : "/minigame1/parts_sc5/sc5p1.target.png",
+        targetWidth: 95,
+        targetHeight: 219
+      },
+      {
+        base : "/minigame1/parts_sc5/sc5p2.png",
+        target : "/minigame1/parts_sc5/sc5p2.target.png",
+        targetWidth: 152,
+        targetHeight: 370
+      },
+      {
+        base : "/minigame1/parts_sc5/sc5p3.png",
+        target : "/minigame1/parts_sc5/sc5p3.target.png",
+        targetWidth: 432,
+        targetHeight: 376,
+      },
+      {
+        base : "/minigame1/parts_sc5/sc5p4.png",
+        target : "/minigame1/parts_sc5/sc5p4.target.png",
+        targetWidth: 139,
+        targetHeight: 342
+      },
+      {
+        base : "/minigame1/parts_sc5/sc5p5.png",
+        target : "/minigame1/parts_sc5/sc5p5.target.png",
+        targetWidth: 438,
+        targetHeight: 684
+      }
+    ]
+  }
+}
+
+export const PROPULSION_SYSTEMS = [
+  rocket_engine, warp_dive, nuclear_propulsion, solar_sails, ion_engine, antimatter
+]
--- frontend/src/components/splash/index.js
+++ frontend/src/components/splash/index.js
@@ -1,59 +1,13 @@
 import React from 'react';
 import './index.css';
-
-const warp_dive = {
-    id: 6,
-    name: "WARP DRIVE",
-    descripcion: "Remember Star Trek? Miguel Alcubierre theorised that it is possible to navigate through the universe by folding the space-time right behind you to create thrust. It should be possible to travel faster than the speed of light and, well, according to Einstein's equations... it should work!",
-    image: "/spacecrafts/sc1.png",
-    minigame1 : {
-      parts : [
-        {
-          base : "/minigame1/parts_sc6/sc6p0.png",
-          target : "/minigame1/parts_sc6/sc6p0.target.png",
-          targetWidth: 500,
-          targetHeight: 500
-        },
-        {
-          base : "/minigame1/parts_sc6/sc6p1.png",
-          target : "/minigame1/parts_sc6/sc6p1.target.png",
-          targetWidth: 205,
-          targetHeight: 625
-        },
-        {
-          base : "/minigame1/parts_sc6/sc6p2.png",
-          target : "/minigame1/parts_sc6/sc6p2.target.png",
-          targetWidth: 220,
-          targetHeight: 650
-        },
-        {
-          base : "/minigame1/parts_sc6/sc6p3.png",
-          target : "/minigame1/parts_sc6/sc6p3.target.png",
-          targetWidth: 230,
-          targetHeight: 650
-        },
-        {
-          base : "/minigame1/parts_sc6/sc6p4.png",
-          target : "/minigame1/parts_sc6/sc6p4.target.png",
-          targetWidth: 500,
-          targetHeight: 780
-        },
-      ]
-    }
-  }
-
-const PROPULSION_SYSTEMS = [
-  warp_dive
-]
+import {PROPULSION_SYSTEMS} from "./engines.js"
 
 function Splash(props) {
   const {onReady} = props
 
   function chooseRandomPropulsionSystem() {
-    let randomInteger = 1 + Math.floor(Math.random() * 6)
-    return PROPULSION_SYSTEMS[0]
-    // TODO: implementar los otros spacecrafts en el minijuego 1
-    // return PROPULSION_SYSTEMS[randomInteger]
+    let randomInteger = Math.floor(Math.random() * 6)
+    return PROPULSION_SYSTEMS[randomInteger]
   }
 
   const propulsionObject = chooseRandomPropulsionSystem()
@@ -65,12 +19,12 @@ function Splash(props) {
           <div style={{flex:1, marginRight:"15px"}}>
             <p>
               Go on a journey is starting from designing and making your own spacecraft from scratch to travelling accross the universe trying to survive!
-              <ol>
-                <li>Draw the parts you need to build a {propulsionObject.name} </li>
-                <li>Assemble the parts to begin the journey</li>
-                <li>Survive travelling through the universe</li>
-              </ol> 
             </p>
+            <ol>
+              <li>Draw the parts you need to build a {propulsionObject.name} </li>
+              <li>Assemble the parts to begin the journey</li>
+              <li>Survive travelling accross the universe</li>
+            </ol> 
             <p>
               Your propulsion system for this journey is going to be <b>{propulsionObject.name}</b>! Click button to start <br/>
             </p>
@@ -79,8 +33,8 @@ function Splash(props) {
           <div className="card" style={{flex:1, backgroundColor:"white", marginLeft:"15px"}}>
             <div className="card-title">{propulsionObject.name}</div>
             <div className="card-container">
-              {propulsionObject.descripcion} <br/>
-              <img src={propulsionObject.image} height="500" />
+              {propulsionObject.description} <br/>
+              <img src={propulsionObject.image} height="500" alt="propulsion system" />
             </div>
           </div>
         </div>
