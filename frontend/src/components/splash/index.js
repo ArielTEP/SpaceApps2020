1833d1f3959db488454c5e508b1b0a57eb43f8d7
peguerosdc <peguerosdc@gmail.com>
2020-10-04 14:39:58 -0500

    Intro with metadata of prop system

--- frontend/src/components/splash/index.js
+++ frontend/src/components/splash/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.css';
 
-const PROPULSION_SYSTEMS = [
-  {
+const warp_dive = {
     id: 6,
     name: "WARP DRIVE",
+    descripcion: "Remember Star Trek? Miguel Alcubierre theorised that it is possible to navigate through the universe by folding the space-time right behind you to create thrust. It should be possible to travel faster than the speed of light and, well, according to Einstein's equations... it should work!",
+    image: "/spacecrafts/sc1.png",
     minigame1 : {
       parts : [
         {
@@ -40,6 +41,9 @@ const PROPULSION_SYSTEMS = [
       ]
     }
   }
+
+const PROPULSION_SYSTEMS = [
+  warp_dive
 ]
 
 function Splash(props) {
@@ -57,14 +61,29 @@ function Splash(props) {
     <div className="splash-container">
       <div style={{flex:1}}>
         <h1>Propulsion Craft</h1>
-        <p>
-          TODO: poner instrucciones
-        </p>
-        <p>
-          Your propulsion system for this journey is going to be <b>{propulsionObject.name}</b>! Click button to start <br/>
-          TODO: poner una descripcion del {propulsionObject.name}
-        </p>
-        <button onClick={() => {onReady(propulsionObject)} }>I wanna go on an adventure</button>
+        <div style={{display:"flex", flexDirection:"row", padding:"0px 150px 0px 150px"}}>
+          <div style={{flex:1, marginRight:"15px"}}>
+            <p>
+              Go on a journey is starting from designing and making your own spacecraft from scratch to travelling accross the universe trying to survive!
+              <ol>
+                <li>Draw the parts you need to build a {propulsionObject.name} </li>
+                <li>Assemble the parts to begin the journey</li>
+                <li>Survive travelling through the universe</li>
+              </ol> 
+            </p>
+            <p>
+              Your propulsion system for this journey is going to be <b>{propulsionObject.name}</b>! Click button to start <br/>
+            </p>
+            <button onClick={() => {onReady(propulsionObject)} }>I wanna go on an adventure</button>
+          </div>
+          <div className="card" style={{flex:1, backgroundColor:"white", marginLeft:"15px"}}>
+            <div className="card-title">{propulsionObject.name}</div>
+            <div className="card-container">
+              {propulsionObject.descripcion} <br/>
+              <img src={propulsionObject.image} height="500" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
